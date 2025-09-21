a0d02c883cc38b1f4396cf53d0e63e85d2bf8b82
Mary Hellen Otinga <maryotinga98@gmail.com>
2025-09-21 14:15:35 +0400

    Update index.ts

--- components/components/interfaces/index.ts
+++ components/components/interfaces/index.ts
@@ -21,3 +21,12 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
+export interface PillProps {
+  title: string
+}
+
+export interface ButtonProps {
+  title: string
+  className?: string
+}
